d3677f638d7a763eb3f7caa2ba9a516023a5a2e5
TheLastVigil <idanflishman@gmail.com>
2024-01-29 19:15:57 +0200

    done

--- html-creator.js
+++ html-creator.js
@@ -1,1 +1,15 @@
-// what is this?
+const { exec } = require('child_process');
+
+function htmlOpener(searchValue) {
+    exec(`start chrome https://developer.mozilla.org/en-US/docs/Web/HTML/Element/${searchValue}`, (err, stdout, stderr) => {
+        if (err) {
+          console.error(`exec error: ${err}`);
+          return;
+        }
+      
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      });
+}
+
+module.exports = htmlOpener;
--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const googleIt = require('./google-it');
+const googleIt = require("./google-it");
+const htmlOpener = require("./html-creator");
 
-const [,,...cliArgs] = process.argv;
+const [, , ...cliArgs] = process.argv;
 if (!cliArgs[0]) {
-    console.error("please enter a value");
-    return;
+  console.error("please enter a value");
+  return;
 }
-googleIt(cliArgs[0])
+switch (cliArgs[0]) {
+  case "google":
+    if (!cliArgs[1]) {
+        console.error("please enter a second value");
+        return
+    }
+    googleIt(cliArgs[1]);
+    break;
+  case "html":
+    if (!cliArgs[1]) {
+        console.error("please enter a second value");
+        return
+    }
+    htmlOpener(cliArgs[1]);
+    break;
+  default:
+    console.error(
+      "value does not exist use either 'google' or 'html' as the first value"
+    );
+    break;
+}
